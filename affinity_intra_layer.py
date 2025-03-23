58dc4534ba604eaaa75742dca7fbbcb2eff8891b
Jasmine010202 <hy221128@163.com>
2025-03-23 20:04:56 +0800

    second commit

---
+++ affinity_intra_layer.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import seaborn as sns
+
+    
+def plot_experts_affinity(experts_selection_stats, fig_dir, num_of_prompts):
+    save_dir = os.path.join(fig_dir, num_of_prompts)
+    os.makedirs(save_dir, exist_ok=True)
+
+    num_layers, num_experts_pre_layer, _ = experts_selection_stats.shape
+
+    for layer in range(num_layers):
+        filename = os.path.join(save_dir, f"experts_affinity_of_layer{layer}.png")
+
+        fig, ax = plt.subplots(figsize=(12,10))
+        #plt.figure(figsize=(16, 14)) #10 8
+        # sns.heatmap(
+        #     experts_selection_stats[layer], annot=True, xticklabels=range(num_experts_pre_layer), yticklabels=range(num_experts_pre_layer), 
+        #     cmap="YlGnBu", fmt=".2f"
+        # )
+
+        sns.heatmap(
+            experts_selection_stats[layer], annot=False, 
+            cmap="YlGnBu", linewidths=0.5, square=True, 
+            cbar_kws={"shrink": 0.5}, ax=ax
+        )
+
+        ax.set_xticks(np.arange(num_experts_pre_layer))
+        ax.set_xticklabels(np.arange(num_experts_pre_layer), rotation=90, fontsize=7)  # 旋转90°防止重叠
+        ax.set_yticks(np.arange(num_experts_pre_layer))
+        ax.set_yticklabels(np.arange(num_experts_pre_layer), fontsize=7)
+
+        plt.title(f"Experts Affinity Intra layer — layer{layer}", fontsize=14)
+        plt.xlabel(f"Experts ID", fontsize=12)
+        plt.ylabel(f"Experts ID", fontsize=12)
+
+        plt.subplots_adjust(left=0.15, right=0.95, top=0.92, bottom=0.15)
+
+        plt.savefig(filename, dpi=300, bbox_inches="tight")
+        plt.close()
+
+
+if __name__ == "__main__":
+
+    model_name = "OLMoE"#Switch_Transformer OLMoE
+    input_name = "gigaword"
+    phrase_mode = "decode" #decode
+    prompt_nums = [8, 16, 32, 64, 128, 256, 512, 1024] # 8, 16, 32, 64, 128, 256, 512, 1024
+    top_k = 8 # ST:1,OL:8
+    num_of_experts_pre_layer = 64
+
+    fig_dir = f"affinity_figs/intra_layer/{model_name}/{input_name}/top{top_k}/{phrase_mode}"
+    os.makedirs(fig_dir, exist_ok=True)
+
+    for num in prompt_nums:
+        routing_data = np.load(f'expert_trace/{model_name}/{input_name}/top{top_k}/{phrase_mode}_routing_trace_{num}.npy')
+        
+        num_tokens, num_layers, _ = routing_data.shape
+        experts_selection_stats = np.zeros((num_layers, num_of_experts_pre_layer, num_of_experts_pre_layer))
+
+        for layer in range(num_layers):
+            for token in range(num_tokens):
+                experts_intra_layer = routing_data[token,layer]
+                for i in range(top_k):
+                    for j in range (i+1, top_k):
+                        expert_i, expert_j = experts_intra_layer[i], experts_intra_layer[j]
+                        experts_selection_stats[layer, expert_i, expert_j] += 1
+                        experts_selection_stats[layer, expert_j, expert_i] += 1 # 对称矩阵
+        
+        plot_experts_affinity(experts_selection_stats, fig_dir, str(num))
---
+++ routing_trace_OLMoE.py
@@ -0,0 +1,82 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "9"
+
+from transformers import AutoTokenizer
+from src.modeling_olmoe import OlmoeForCausalLM
+
+import torch
+# import os
+import numpy as np
+import random 
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Load different ckpts via passing e.g. `revision=step10000-tokens41B`
+
+tokenizer = AutoTokenizer.from_pretrained("./models/OLMoE-1B-7B-0924")
+model = OlmoeForCausalLM.from_pretrained("./models/OLMoE-1B-7B-0924", torch_dtype=torch.float16).to(DEVICE) #, num_experts_per_tok=1
+
+with open("./dataset/gigaword_test.txt", "r") as file:
+    prompts = [line.strip() for line in file.readlines() if line.strip()]
+
+prompt_sizes = [8, 16, 32, 64, 128, 256, 512, 1024] 
+
+output_dir = "./output/OLMoE/gigaword/top8"
+os.makedirs(output_dir, exist_ok=True)
+
+routing_data_dir = "./expert_trace/OLMoE/gigaword/top8"
+os.makedirs(routing_data_dir, exist_ok=True)
+
+for size in prompt_sizes:
+    sampled_prompts = random.sample(prompts, size)
+    output_file_path =  f"{output_dir}/output_{size}.txt"
+    
+    model.experts_routing_trace = []  
+
+    with open(output_file_path, "w") as output_file:
+        for prompt in sampled_prompts:
+            inputs = tokenizer(prompt, return_tensors="pt")
+            inputs = {k: v.to(DEVICE) for k, v in inputs.items()}
+            # outputs = model.generate(**inputs, max_length=64)
+            outputs = model.generate(**inputs, max_new_tokens=50)
+
+            # print(tokenizer.decode(outputs[0]))
+            output_file.write(f"Prompt: {prompt}\nGenerated Summary: {tokenizer.decode(outputs[0])}\n")
+    
+    print(f"End of inference.({size} prompts)")
+
+    # 保存路由信息
+    with open(output_file_path, "a") as output_file:
+        output_file.write(f"routing_trace:\n{model.experts_routing_trace}\n")
+
+    if model.experts_routing_trace:
+        routing_trace_data = torch.cat(model.experts_routing_trace, dim=0).cpu().numpy()
+        #routing_trace_data = np.concatenate(model.experts_routing_trace, axis=0)
+        #torch.cat(model.experts_routing_trace, dim=0).cpu().numpy()
+        #encode_trace = np.concatenate(model.encode_routing_trace, axis=0)
+        np.save(f"{routing_data_dir}/decode_routing_trace_{size}.npy", routing_trace_data)
+        print(f"Saved: {routing_data_dir}/decode_routing_trace_{size}.npy")
+
+
+
+# inputs = tokenizer("Bitcoin is", return_tensors="pt")
+# inputs = {k: v.to(DEVICE) for k, v in inputs.items()}
+
+# out = model.generate(**inputs, max_length=64)
+
+# print(tokenizer.decode(out[0]))
+
+# routing_trace_data = torch.cat(model.experts_routing_trace, dim=0).cpu().numpy()
+
+# print(routing_trace_data)
+# print(routing_trace_data.shape)
+
+
+# routing_data_dir = "./expert_trace/OLMoE/test"
+# os.makedirs(routing_data_dir, exist_ok=True)
+
+# np.save(f"{routing_data_dir}/decode_routing_trace_top1_test.npy", routing_trace_data)
+
+
+
+
